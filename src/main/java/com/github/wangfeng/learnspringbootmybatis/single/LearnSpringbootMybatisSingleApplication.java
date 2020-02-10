ffc380da595712cc941fecf52f28d99afcfd7a38
wangfeng <wangfengbabe@163.com>
2020-02-10 16:09:18 +0800

    init

---
+++ src/main/java/com/github/wangfeng/learnspringbootmybatis/single/LearnSpringbootMybatisSingleApplication.java
@@ -0,0 +1,13 @@
+package com.github.wangfeng.learnspringbootmybatis.single;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootMybatisSingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringbootMybatisSingleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wangfeng/learnspringbootmybatis/single/LearnSpringbootMybatisSingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wangfeng.learnspringbootmybatis.single;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootMybatisSingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
