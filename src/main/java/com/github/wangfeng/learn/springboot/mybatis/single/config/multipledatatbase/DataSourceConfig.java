3eb678ceb63f82d5633a45ea66f15bd61fe1cbed
wangfeng <wangfengbabe@163.com>
2020-03-03 14:37:30 +0800

    多数据源配置
    url--->jdbcUrl

--- src/main/java/com/github/wangfeng/learn/springboot/mybatis/single/config/multipledatatbase/DataSourceConfig.java
+++ src/main/java/com/github/wangfeng/learn/springboot/mybatis/single/config/multipledatatbase/DataSourceConfig.java
@@ -3,6 +3,7 @@ package com.github.wangfeng.learn.springboot.mybatis.single.config.multipledatat
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,25 +12,29 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    @Bean("ds1DataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource.ds1")
-    public DataSourceProperties ds1DataSourceProperties() {
-        return new DataSourceProperties();
-    }
+//    @Bean("ds1DataSourceProperties")
+//    @ConfigurationProperties(prefix = "spring.datasource.ds1")
+//    public DataSourceProperties ds1DataSourceProperties() {
+//        return new DataSourceProperties();
+//    }
 
     @Bean("ds1DataSource")
-    public DataSource ds1DataSource(@Qualifier("ds1DataSourceProperties") DataSourceProperties dataSourceProperties) {
-        return dataSourceProperties.initializeDataSourceBuilder().build();
+    @ConfigurationProperties(prefix = "spring.datasource.ds1")
+    public DataSource ds1DataSource() {
+//        return dataSourceProperties.initializeDataSourceBuilder().build();
+        return DataSourceBuilder.create().build();
     }
 
-    @Bean("ds2DataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource.ds2")
-    public DataSourceProperties ds2DataSourceProperties() {
-        return new DataSourceProperties();
-    }
+//    @Bean("ds2DataSourceProperties")
+//    @ConfigurationProperties(prefix = "spring.datasource.ds2")
+//    public DataSourceProperties ds2DataSourceProperties() {
+//        return new DataSourceProperties();
+//    }
 
     @Bean("ds2DataSource")
-    public DataSource ds2DataSource(@Qualifier("ds2DataSourceProperties") DataSourceProperties dataSourceProperties) {
-        return dataSourceProperties.initializeDataSourceBuilder().build();
+    @ConfigurationProperties(prefix = "spring.datasource.ds2")
+    public DataSource ds2DataSource() {
+//        return dataSourceProperties.initializeDataSourceBuilder().build();
+        return DataSourceBuilder.create().build();
     }
 }
